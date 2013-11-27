70c4f67d39f6e045e085d128020e084a21a0584d
wiredeltainc <contact@wiredelta.com>
2013-11-27 16:59:00 +0000

    Update device.js

--- js/device.js
+++ js/device.js
@@ -1,6 +1,8 @@
-document.addEventListener("deviceready", onDeviceReady, false);
 
-    	function onDeviceReady(){
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady(){
         
         window.requestFileSystem(
          LocalFileSystem.PERSISTENT, 0, 
@@ -9,9 +11,9 @@ document.addEventListener("deviceready", onDeviceReady, false);
              window.fileSystem = fileSystem;
          }, fail);
 
-    	}
+    }
 
-    	function existsFile(fileName,dwdURL){
+    function existsFile(fileName,dwdURL){
         
         fileSystem.root.getFile(fileName, {
             
@@ -20,40 +22,34 @@ document.addEventListener("deviceready", onDeviceReady, false);
             function(f){
                 
                 console.log(f);
-                
                  alert("file founded: " + f.toURI());
-                 
                   showLink(f.toURI());
             },
             function(){
                 window.confirm("would you like to download..?");
-                
                     downloadFile(fileName,dwdURL);
             
             })
 
-    	}
+    }
 
-    	function downloadFile(fileName,dwdURL) {
+    function downloadFile(fileName,dwdURL) {
         var fileTransfer = new FileTransfer();
         
         fileTransfer.download(dwdURL, 
         currentDir+"/"+fileName,
            function(theFile) {
-           	
                alert("download complete: " + theFile.toURI());
-               
                showLink(theFile.toURI());
            },
            function(error) {
-           	
             alert("download error source " + error.source);
             alert("download error target " + error.target);
             alert("upload error code: " + error.code);
            });
-    	}
+    }
  
-    	function showLink(url){
+    function showLink(url){
         
         alert(url);
         
@@ -62,31 +58,28 @@ document.addEventListener("deviceready", onDeviceReady, false);
         //ft.src=url;
         
         $(document).ready(function(){
-                
-                $("#pg_one").hide();
-                
-                $("#pg_two").show();
-                
+        	
+        	$("#pg_one").hide();
+        	
+        	$("#pg_two").show();
+        	
         });
        
- 
-    	}
+    }
      
  
-    	function fail(evt) {
-    		
-        	console.log(evt.target.error.code);
-    	}
+    function fail(evt) {
+        console.log(evt.target.error.code);
+    }
     
-    	$(document).ready(function(){
-            
-            $("#pg_two").hide();
-            
-            $("#b2p").click(function(){
-            
-            $("#pg_two").hide();
-            
-            $("#pg_one").show();
-                    
-            });
-    	})
+    $(document).ready(function(){
+    	
+    	$("#pg_two").hide();
+    	
+    	$("#b2p").click(function(){
+    	
+    	$("#pg_two").hide();
+    	$("#pg_one").show();
+    		
+    	});
+    })
